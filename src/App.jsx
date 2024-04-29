ca9781d751a428559203d351f7539f19d5f90faf
Babalola-Yusuf <yusufbabalola01@gmail.com>
2024-04-29 13:05:55 +0100

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [cars, setCar] = useState([])
+  const [carYear, setCarYear] = useState(new Date().getFullYear());
+  const [carMake, setCarMake] = useState("");
+  const [carModel, setCarModel] = useState("");
+
+  const handleAddCar=()=>{
+    const newCar = {year:carYear, make:carMake, model:carModel}
+    setCar(c =>[...c, newCar])
+  }
+
+  const handleRemoveCar = (index) =>{
+
+  }
+
+  const handleYearChange = (event) =>{
+    setCarYear(event.target.value)
+  }
+
+  const handleMakeChange = (event) =>{
+    setCarMake(event.target.value)
+  }
+
+  const handleModelChange = (event) =>{
+    setCarModel(event.target.value)
+  }
+
+  return (
+    
+      <div className=''>
+        <h1>List of car objects</h1>
+        <ul>
+          {cars.map((car, index)=>
+            <li key={index}>{car.year}  {car.make} {car.model}</li>
+          )}
+        </ul>
+       <input type="number" onChange={handleYearChange} value={carYear} className='border-2' placeholder='input year'/>
+       <input type="text" onChange={handleMakeChange} value={carMake} className='border-2' placeholder='input car make'/>
+       <input type="text" onChange={handleModelChange} value={carModel} className=' border-2' placeholder='input car model'/>
+       <button onClick={handleAddCar} className='border-2'>Add car</button>
+      </div>
+  )
+}
+
+export default App
